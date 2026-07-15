38d84aa03cfe98e2a18912abda6653c5144b0a30
Jesse-Aerts <jesse.aerts@uhasselt.be>
2026-07-15 15:19:18 +0200

    updating

--- allen_cahn.py
+++ allen_cahn.py
@@ -44,19 +44,19 @@ import pyvistaqt as pvqt
 import shutil
    
 
-def allen_cahn():
+def allen_cahn(type_of_linearisation = "newton", nb_of_spatial_steps =50, nb_of_time_steps = 100, final_time = 1, eps = 10**(-4)):
     """
     =============================================================
     1. PARAMETER DEFINITIONS
     =============================================================
     """
 
-    type_of_linearisation = "newton"                            #Scheme: "newton" (quadratic convergence), "L" (simple linear), or "M" (modified stabilized)
-    nb_of_spatial_steps = 50                                    #Mesh resolution (number of segments per boundary edge)
-    nb_of_time_steps=100                                        #Total number of time steps (N_T)
+    #type_of_linearisation = type_of_linearisation               #Scheme: "newton" (quadratic convergence), "L" (simple linear), or "M" (modified stabilized)
+    #nb_of_spatial_steps = 50                                    #Mesh resolution (number of segments per boundary edge)
+    #nb_of_time_steps=100                                        #Total number of time steps (N_T)
     T = 1                                                       #Total simulation time (T)
     dt = T/nb_of_time_steps                                     #Time step size (dt)
-    eps = 10**(-4)                                              #Interfacial width parameter (epsilon^2)
+    #eps = 10**(-4)                                              #Interfacial width parameter (epsilon^2)
     M = 0.1                                                     #Stabilization constant used only in the "M" scheme                                
     t = 0.0 
 
@@ -147,19 +147,19 @@ def allen_cahn():
 
 
     # --- 1. Clean and Prepare the Output Directory ---
-    output_folder = "results"
-
     if msh.comm.rank == 0:
         # If the folder already exists, delete it and all its contents
-        if os.path.exists(output_folder):
-            shutil.rmtree(output_folder)
+        if os.path.exists("graphs"):
+            shutil.rmtree("graphs")
+        if os.path.exists("outputs"):
+            shutil.rmtree("outputs")
         # Create a fresh, empty directory
-        os.makedirs(output_folder, exist_ok=True)
+        os.makedirs("graphs", exist_ok=True)
+        os.makedirs("outputs", exist_ok=True)
 
     # MPI Barrier: Forces all processor ranks to wait until rank 0 
     # is done deleting and recreating the directory.
     msh.comm.Barrier()
-
 
     # --- 2. PyVista Mesh and Plotter Setup ---
     topology, cell_types, x = plot.vtk_mesh(V)
@@ -171,15 +171,9 @@ def allen_cahn():
     grid.point_data["c"] = c_visual.x.array.real
     grid.set_active_scalars("c")
 
-    p = pvqt.BackgroundPlotter(title="concentration", auto_update=True)
-    p.add_mesh(grid, clim=[-1, 1], cmap="viridis")
-    p.view_xy(negative=True)
-    p.add_text(f"time: {t}", font_size=12, name="timelabel")
-
-
     # --- 3. Initialize the VTK Writer ---
     u.name = "c"
-    vtk_filepath = os.path.join(output_folder, "output_solution.pvd")
+    vtk_filepath = os.path.join("graphs","output_solution.pvd")
     vtk_file = dolfinx.io.VTKFile(msh.comm, vtk_filepath, "w")
 
 
@@ -191,12 +185,13 @@ def allen_cahn():
 
     errors = []
     energies = []
+    iterations = []
 
     solution_previous_time.x.array[:] = u0.x.array[:]
     solution_previous_iteration.x.array[:] = u0.x.array[:]
     step = 1
     error = 1
-    iteration = 1
+    nb_of_iterations = 0
     while t < T:
         t += dt
         while error > 10**(-8):
@@ -213,9 +208,13 @@ def allen_cahn():
             print(energy)
 
             solution_previous_iteration.x.array[:] = u.x.array[:]
+            nb_of_iterations += 1
         
         solution_previous_time.x.array[:] = u.x.array[:]
         solution_previous_iteration.x.array[:] = u.x.array[:]
+
+        iterations.append(nb_of_iterations)
+        nb_of_iterations = 0
 
         vtk_file.write_function(u, t)
 
@@ -226,6 +225,7 @@ def allen_cahn():
     vtk_file.close()
     errors_path = os.path.join("outputs", "errors.txt")
     energies_path = os.path.join("outputs", "energies.txt")
+    iterations_path = os.path.join("outputs", "iterations.txt")
 
     # Write errors to errors.txt
     with open(errors_path, "w") as f:
@@ -237,6 +237,13 @@ def allen_cahn():
         for energy in energies:
             f.write(f"{energy}\n")
     vtk_file.close()
+
+    with open(iterations_path, "w") as f:
+        for iteration in iterations:
+            f.write(f"{iteration}\n")
+
+
+
     return errors, energies
 
 
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import cahn_hilliard as ch
 import allen_cahn as ac
 from plot import plot2d, plot3d, animation, plot_errors, plot_energies, plot_errors2, plot_energies2, plot_errors3, plot_errors4, plot_energies4
 
+
+errors, energies = ac.allen_cahn(
+    type_of_linearisation = "newton",              #Scheme: "newton" (quadratic convergence), "L" (simple linear), or "M" (modified stabilized)
+    nb_of_spatial_steps = 50,                                   #Mesh resolution (number of segments per boundary edge)
+    nb_of_time_steps=100,                                       #Total number of time steps (N_T)
+    eps = 10**(-4)                                              #Interfacial width parameter (epsilon^2)
+)
 #errors, energies = ch.cahn_hilliard()
 
 """
